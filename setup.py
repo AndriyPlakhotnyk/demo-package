6743f147554dd903d5638182a20238a50a391b36
AndriyPlakhotnyk <andriy.plakhotnyk@ucalgary.ca>
2023-03-22 11:47:24 -0600

    init

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+config = {
+    'description': 'TEST',
+    'author': 'andriy',
+    'url': 'https://github.com/AndriyPlakhotnyk',
+    'download_url': 'https://github.com/AndriyPlakhotnyk/andriy',
+    'author_email': 'andriy.plakhotnyk@ucalgay.ca',
+    'version': '0.0.1',
+    'install_requires': [],
+    'packages': find_packages(),
+    'scripts': [],
+    'name': 'andriy'
+}
+
+setup(**config)
